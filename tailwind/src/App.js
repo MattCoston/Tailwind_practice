884f8987763b9b1d08c851a76d38eab1d2d7e622
elliotm77 <mattcoston7@gmail.com>
2021-02-06 18:23:49 -0600

    Creat react app and tailwind failure

---
+++ tailwind/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" class="hover:bg-gray-50"></input>
+        <input type="text"></input>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ultricies at enim sit amet ultricies. Nam imperdiet facilisis maximus. Vivamus diam ligula, pellentesque ut libero vel, semper imperdiet arcu. Nulla tellus lectus, pretium ut porttitor vel, commodo vitae libero. Cras dictum at ligula ut tempus. Phasellus finibus facilisis ultrices. Phasellus fermentum risus sed risus fringilla pulvinar. Nulla in est luctus, faucibus nulla non, mattis velit. Morbi in erat nec lorem interdum vestibulum. Donec sodales lacinia consequat. Integer vel commodo est. Proin feugiat libero non odio varius pulvinar.</p>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
